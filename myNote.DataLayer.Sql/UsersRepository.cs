5579915530056385369497d45b266d3ef4c1126b
MaxVortman <maka.abort@gmail.com>
2017-10-14 21:23:58 +0300

    Refactored User and UsersRepository with linq.

--- myNote.DataLayer.Sql/UsersRepository.cs
+++ myNote.DataLayer.Sql/UsersRepository.cs
@@ -1,6 +1,7 @@
 ﻿using myNote.Model;
 using System;
 using System.Collections.Generic;
+using System.Data.Linq;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
@@ -21,32 +22,11 @@ namespace myNote.DataLayer.Sql
 
         public User CreateUser(User user)
         {
-            using (var sqlConnection = new SqlConnection(connectionString))
-            {
-                sqlConnection.Open();
-                using (var command = sqlConnection.CreateCommand())
-                {
-                    command.CommandText = "insert into Users (Id, Name, Birthday, Email, RegisterDate) values (@Id, @Name, @Birthday, @Email, @RegisterDate)";
-                    user.Id = Guid.NewGuid();
-                    command.Parameters.AddWithValue("@Id", user.Id);
-                    command.Parameters.AddWithValue("@Name", user.Name);
-                    command.Parameters.AddWithValue("@Email", user.Email);
-
-                    if (user.Birthday != default(DateTime))
-                        command.Parameters.AddWithValue("@Birthday", user.Birthday);
-                    else
-                        command.Parameters.AddWithValue("@Birthday", DBNull.Value);
-
-                    if (user.RegisterDate != default(DateTime))
-                        command.Parameters.AddWithValue("@RegisterDate", user.RegisterDate);
-                    else
-                        command.Parameters.AddWithValue("@RegisterDate", DBNull.Value);
-
-                    command.ExecuteNonQuery();
-
-                    return user;
-                }
-            }
+            var db = new DataContext(connectionString);
+            user.Id = Guid.NewGuid();
+            db.GetTable<User>().InsertOnSubmit(user);
+            db.SubmitChanges();
+            return user;
         }
 
         public void DeleteUser(Guid id)
@@ -65,35 +45,15 @@ namespace myNote.DataLayer.Sql
 
         public User GetUser(Guid id)
         {
-            using (var sqlConnection = new SqlConnection(connectionString))
-            {
-                sqlConnection.Open();
-                using (var command = sqlConnection.CreateCommand())
-                {
-                    command.CommandText = "select * from Users where Id = @Id";
-                    command.Parameters.AddWithValue("@Id", id);
+            var db = new DataContext(connectionString);
 
-                    using (var reader = command.ExecuteReader())
-                    {
-                        if (!reader.Read())
-                            throw new ArgumentException($"Пользователь с id {id} не найден");
-                        
-                        var user = new User
-                        {                            
-                            Id = reader.GetGuid(reader.GetOrdinal("Id")),
-                            Name = reader.GetString(reader.GetOrdinal("Name")),
-                            Email = reader.GetString(reader.GetOrdinal("Email")),
-                        };
-                        if (!reader.IsDBNull(reader.GetOrdinal("Birthday")))
-                            user.Birthday = reader.GetDateTime(reader.GetOrdinal("Birthday"));
-                        if (!reader.IsDBNull(reader.GetOrdinal("RegisterDate")))
-                            user.Birthday = reader.GetDateTime(reader.GetOrdinal("RegisterDate"));
-                        
-                        user.UserGroups = groupsRepository.GetUserGroups(user.Id);
-                        return user;
-                    }
-                }
-            }
+            var user = (from u in db.GetTable<User>()
+                        where u.Id == id
+                        select u).FirstOrDefault();
+            if (user == default(User))            
+                throw new ArgumentException($"Пользователь с id {id} не найден");
+            user.UserGroups = groupsRepository.GetUserGroups(user.Id);
+            return user;
         }
     }
 }
--- myNote.Model/User.cs
+++ myNote.Model/User.cs
@@ -1,19 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Linq.Mapping;
 
 namespace myNote.Model
 {
+    [Table(Name = "Users")]
     public class User
     {
+        [Column(IsPrimaryKey = true)]
         public Guid Id { get; set; }
-
+        [Column]
         public string Name { get; set; }
-
-        public DateTime Birthday { get; set; }
-
+        [Column]
+        public DateTime? Birthday { get; set; }
+        [Column]
         public string Email { get; set; }
-
-        public DateTime RegisterDate { get; set; }
+        [Column]
+        public DateTime? RegisterDate { get; set; }
 
         public IEnumerable<Group> UserGroups { get; set; }
     }
