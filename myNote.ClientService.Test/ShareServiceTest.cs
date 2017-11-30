f8eaf7a3fbd40761f03a101e1e84f9d890c1d01c
MaxVortman <maka.abort@gmail.com>
2017-11-30 16:30:25 +0300

    getsomeshares already tested

--- myNote.ClientService.Test/ShareServiceTest.cs
+++ myNote.ClientService.Test/ShareServiceTest.cs
@@ -54,6 +54,34 @@ namespace myNote.ClientService.Test
             Assert.AreEqual(share.NoteId, userSharesFromApi.First().Id);
         }
 
+        [TestMethod]
+        public void ShouldGetSomeShares()
+        {
+            //arrange
+            const int COUNT = 10;
+            var credential = new Credential { Login = "MaxTest", Password = PasswordCrypter.GetPasswordMD5Hash("kljkljkl") };
+            loginClient.Register(credential);
+            var token = loginClient.LoginAsync(credential).Result;
+            tempUser.Add(credential.Login, token);
+
+            var noteService = new NoteService(ConnectionString);
+            var shareService = new ShareService(ConnectionString);
+
+            Note note;
+            Share share;
+            for (int i = 0; i < COUNT; i++)
+            {
+                note = noteService.CreateNoteAsync(new Note { UserId = token.UserId }, token).Result;
+                share = shareService.CreateShareAsync(note, token).Result; 
+            }
+
+            //act
+            var sharesFromApi = shareService.GetSomeShares(COUNT).Result;
+
+            //assert
+            Assert.AreEqual(COUNT, sharesFromApi.Count());
+        }
+
         #region Clean Up temp
 
         [TestCleanup]
