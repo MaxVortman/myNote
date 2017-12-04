6f2d87aac63ac8fb7de29a48b1fabf77acabee74
MaxVortman <maka.abort@gmail.com>
2017-12-04 14:19:04 +0300

    Complete save user changes.

--- myNote.WPFClient/ViewModel/UserContentViewModel.cs
+++ myNote.WPFClient/ViewModel/UserContentViewModel.cs
@@ -1,10 +1,12 @@
-﻿using myNote.Model;
+﻿using myNote.ClientService;
+using myNote.Model;
 using myNote.WPFClient.ViewModel.Base;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace myNote.WPFClient.ViewModel
 {
@@ -15,12 +17,31 @@ namespace myNote.WPFClient.ViewModel
         /// Viewable user
         /// </summary>
         public User User { get; set; }
+        /// <summary>
+        /// Show can we change user or not
+        /// </summary>
+        public bool IsEnabled { get; set; }
+        #endregion
+
+        #region Commands
+        /// <summary>
+        /// Execute when we want to save user changes
+        /// </summary>
+        public ICommand SaveCommand { get; set; }
         #endregion
 
         #region Default Constructor
         public UserContentViewModel(User user)
         {
             User = user;
+            IsEnabled = User.Id == UserData.UserDataContent.Token.UserId;
+            var api = ApiClient.CreateInstance(IoC.IoC.ConnectionString);
+
+            if (IsEnabled)
+                SaveCommand = new RelayCommand(async(obj) =>
+                {
+                    await api.UserService.UpdateUserAsync(User, UserData.UserDataContent.Token);
+                });
         }
         #endregion
     }
