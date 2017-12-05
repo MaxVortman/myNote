34a98dad4ad3fbd40dd06d9fa21eb4175b1b9171
MaxVortman <maka.abort@gmail.com>
2017-12-05 15:57:28 +0300

    Correct mistekes.

--- myNote.Api/Controllers/ImageController.cs
+++ myNote.Api/Controllers/ImageController.cs
@@ -12,19 +12,24 @@ using System.Windows.Media.Imaging;
 namespace myNote.Api.Controllers
 {
     public class ImageController : ApiController
-    {        
+    {
         /// <summary>
         /// Get image from server by path
         /// </summary>
         /// <param name="path">image path on server</param>
-        /// <returns></returns>
+        /// <returns>image byte array</returns>
         [HttpGet]
         [Route("api/image/{path}")]
-        public ImageSource Get(string path)
+        public byte[] Get(string path)
         {
             try
             {
-                return new BitmapImage(new Uri(path));
+                using (var ms = new MemoryStream())
+                {
+                    var image = Image.FromFile(path);
+                    image.Save(ms, System.Drawing.Imaging.ImageFormat.Bmp);
+                    return ms.ToArray();
+                }
             }
             catch (Exception e)
             {
@@ -42,10 +47,13 @@ namespace myNote.Api.Controllers
         [Route("api/image/{userId}")]
         public string Post(byte[] imageBytes, Guid userId)
         {
-            var path = $"pack://application:,,,/Images/{userId}.bmp";
-            var image = Image.FromStream(new MemoryStream(imageBytes));
-            image.Save(path);
-            return path;
+            using (var ms = new MemoryStream(imageBytes))
+            {
+                var path = $"pack://application:,,,/Images/{userId}.bmp";
+                var image = Image.FromStream(ms);
+                image.Save(path);
+                return path; 
+            }
         }
     }
 }
